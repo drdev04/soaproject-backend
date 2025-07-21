111e53bb8c69cb936f5421214881adf247d3c592
MarckTK <perezmatosmarco14@gmail.com>
2025-07-21 02:53:47 -0500

    feat: :sparkles: Implement room addition endpoint and update service layer

--- src/main/java/com/marcktk/lakesidehotel/service/RoomService.java
+++ src/main/java/com/marcktk/lakesidehotel/service/RoomService.java
@@ -3,6 +3,7 @@ package com.marcktk.lakesidehotel.service;
 import com.marcktk.lakesidehotel.model.Room;
 import com.marcktk.lakesidehotel.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.sql.rowset.serial.SerialBlob;
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
 
+@Service
 @RequiredArgsConstructor
 public class RoomService implements IRoomService {
     private final RoomRepository roomRepository;
